d13ce58e0b42ecd945f588e626ae1b9282da178e
anodynos <agelos.pikoulas@gmail.com>
2014-10-08 17:30:36 +0300

    browserify version '3.46.1'

--- timers.js
+++ timers.js
@@ -3,15 +3,15 @@
 From [node2web](http://github.com/anodynos/node2web) collection,
 should/will be exposed as 'timers' to [bower](http://bower.io) for *browser* usage.
 
-browserify version: '3.24.10', build date 'Tue Sep 16 2014 02:22:02 GMT+0300 (EEST)' 
+browserify version: '3.46.1', build date 'Wed Oct 08 2014 17:30:36 GMT+0300 (EEST)' 
 **/
 !function(e){if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.timers=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 // DOM APIs, for completeness
 
-if (typeof setTimeout !== 'undefined') exports.setTimeout = setTimeout;
-if (typeof clearTimeout !== 'undefined') exports.clearTimeout = clearTimeout;
-if (typeof setInterval !== 'undefined') exports.setInterval = setInterval;
-if (typeof clearInterval !== 'undefined') exports.clearInterval = clearInterval;
+if (typeof setTimeout !== 'undefined') exports.setTimeout = function() { return setTimeout.apply(window, arguments); };
+if (typeof clearTimeout !== 'undefined') exports.clearTimeout = function() { clearTimeout.apply(window, arguments); };
+if (typeof setInterval !== 'undefined') exports.setInterval = function() { return setInterval.apply(window, arguments); };
+if (typeof clearInterval !== 'undefined') exports.clearInterval = function() { clearInterval.apply(window, arguments); };
 
 // TODO: Change to more effiecient list approach used in Node.js
 // For now, we just implement the APIs using the primitives above.
